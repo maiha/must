163ea6cd548cba4aaf7ceb950b03b32768f7963e
maiha <maiha@wota.jp>
2013-08-25 18:02:31 +0900

    speed up for the special case: obj.must(klass)

--- lib/must.rb
+++ lib/must.rb
@@ -9,6 +9,10 @@ module Must
 
   def must(*args, &block)
     if args.size > 0
+      # Fast type checking for the case "obj.must(klass)"
+      args.each{|klass| return self if self.class == klass}
+
+      # Or, check it in a slow but strict way
       Rule.new(self).be.kind_of(*args, &block)
     else
       Rule.new(self)
